7fcf4716e5f53a7991d2f56315268ce91b945afc
Alejandro Quiles García <alexqg1208@gmail.com>
2024-11-19 20:15:27 +0000

    Commit Final

--- src/app.js
+++ src/app.js
@@ -6,21 +6,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   let pronoun = ["the", "our", "his"];
-  let adj = ["great", "big", "amazing"];
+  let adjective = ["great", "big", "amazing"];
   let noun = ["jogger", "racoon", "dog", "merchant", "driver"];
 
-  let dominios = document.querySelector("#dominios");
-
-  let AllDominios = "";
-
+  //  let allDomains = [];
+  let allDomains = "";
   for (let a = 0; a < pronoun.length; a++) {
-    for (let b = 0; b < adj.length; b++) {
+    for (let b = 0; b < adjective.length; b++) {
       for (let c = 0; c < noun.length; c++) {
-        let dominio = pronoun[a] + adj[b] + noun[c] + ".com";
+        let domain = pronoun[a] + adjective[b] + noun[c] + ".com";
 
-        AllDominios += dominio + " ";
+        allDomains += `<li class="list-group-item">${domain}</li>`;
+        //  allDomains.push(domain);      }
       }
     }
   }
-  dominios.innerHTML = AllDominios;
+  console.log(allDomains);
+  // const listDomains = allDomains.map(domain => {
+  //    return `<li class="list-group-item">${domain}</li>`;
+  //  });
+  const html = `<ul class="list-group">${allDomains}</ul>`;
+
+  document.querySelector("#domains").innerHTML = html;
 };
